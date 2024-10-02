0316da51339ef467b7e5c6fe7163b94b47300ac7
Sudheesh <sudheeshrs4797@gmail.com>
2024-10-02 11:44:13 +0530

    frist commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const option ={
+    method:'GET',
+    headers: {
+         ' X-RapidAPI-Key':'52953b28fdmsh78e71a4d476e7ccp1a1807jsnf9a3fda26853',
+        'X-RapidAPI-Host':'imdb8.p.rapidapi.com'
+    }
+};
+
+function search(){
+    moviename=movieName . value
+    
+}
