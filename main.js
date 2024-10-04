8f58fedd5d3f23d67f135258f534b4a2d9363234
Sudheesh6690 <sudheeshrs4797@gmail.com>
2024-10-04 07:41:48 +0530

    Second commit

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-const option ={
-    method:'GET',
+const options = {
+    method: 'GET',
     headers: {
-         ' X-RapidAPI-Key':'52953b28fdmsh78e71a4d476e7ccp1a1807jsnf9a3fda26853',
-        'X-RapidAPI-Host':'imdb8.p.rapidapi.com'
+        'X-RapidAPI-Key': '52953b28fdmsh78e71a4d476e7ccp1a1807jsnf9a3fda26853',
+        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
     }
 };
 
-function search(){
-    moviename=movieName . value
-    
+function search() {
+    const moviename = document.getElementById('MovieName').value;
+
+    fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${moviename}`, options)
+        .then(response => response.json())
+        .then(data => {
+            const list = data.d;
+            document.querySelector(".movies").innerHTML = "";
+
+            list.map((item) => {
+                const name = item.l;
+                const poster = item.i.imageUrl;
+                const movie = `
+                    <div class="col-md-4 col-sm-6 mb-4">
+                        <div class="card h-100">
+                            <img src="${poster}" class="card-img-top" alt="${name} poster">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">${name}</h5>
+                            </div>
+                        </div>
+                    </div>`;
+                document.querySelector(".movies").innerHTML += movie;
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
